09d903030acbd66e862c2f2191592d0ec308849b
Darkenn <64686364+Noz155@users.noreply.github.com>
2024-01-17 17:56:33 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 cd /home/container
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval "${MODIFIED_STARTUP}"
+${MODIFIED_STARTUP}
