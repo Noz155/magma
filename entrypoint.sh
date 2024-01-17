5920ca67b393d1497ccd968278a7634c5ae49407
Darkenn <64686364+Noz155@users.noreply.github.com>
2024-01-17 18:07:36 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /home/container
+cd /home/container || exit 1  # Exit if the directory doesn't exist or cannot be accessed
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(eval "echo \"${STARTUP}\"" | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container\$ ${MODIFIED_STARTUP}"  # Escape the dollar sign to avoid variable expansion in echo
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval "${MODIFIED_STARTUP}"  # Use eval to execute the modified command
